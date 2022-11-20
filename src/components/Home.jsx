b2b7b8651294ae05246b51b7c91d9fd5d818fa10
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-11-20 10:05:34 +0300

    feat: add featch

---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+function Home() {
+  const [calendar, setCalendar] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://82.202.204.94/tmp/test.php`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // setCalendar(data.project);
+        // console.log(calendar);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <div data-testid="main-page">fsfsf</div>
+    </>
+  );
+}
+
+export { Home };
